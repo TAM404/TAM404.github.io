c9fd52f5b0e67ba067df5e7ef190ab2b7299a4b0
TAM404 <31936708+TAM404@users.noreply.github.com>
2023-01-24 21:33:44 +0100

    Update markup-engine_v1.js

--- markup-engine_v1.js
+++ markup-engine_v1.js
@@ -1,1 +1,77 @@
 
+function engineV1(str) {
+    newstr = ""
+    str = str.split("\n");
+    for (var i = 0; i < str.length; i++) {
+        line = HTMLescape(str[i]);
+        if (line.length != 0) {
+            line = escaper(line);
+
+            if (line.startsWith("#")) {
+                if (line.startsWith("##")) {
+                    if (line.startsWith("###")) {
+                        // ###
+                        newstr += "<h3>" + line.slice(3) + "</h3>";
+                    } else {
+                        // ##
+                        newstr += "<h2>" + line.slice(2) + "</h2>";
+                    }
+                } else {
+                    // #
+                    newstr += "<h1>" + line.slice(1) + "</h1>";
+                }
+            }
+            else if (line.startsWith("[[[")) {
+                newstr += "<img src=\" " + line.slice(3) + " \">";
+            } else {
+
+                list = line.split("*");
+                line = "";
+                for (var j = 0; j < list.length; j++) {
+                    if (j % 2)
+                        line += "<b>" + list[j] + "</b>";
+                    else
+                        line += list[j];
+                }
+
+                list = line.split("^");
+                line = "";
+                for (var j = 0; j < list.length; j++) {
+                    if (j % 2)
+                        line += "<span style='color:red;'>" + list[j] + "</span>";
+                    else
+                        line += list[j];
+                }
+
+                newstr += line + "</br>";
+
+            }
+        } else {
+            newstr += "</br>";
+        }
+    }
+
+    return newstr;
+}
+
+function HTMLescape(str) {
+    return str.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;");
+}
+
+function HtmlEncode(s) {
+    return "&#" + s.charCodeAt() + ";";
+}
+
+function escaper(str) {
+
+    str2 = ""
+    for (var i = 0; i < str.length - 1; i++) {
+        if (str[i] == '\\') {
+            str2 += HtmlEncode(str[i + 1]);
+            i++;
+        } else {
+            str2 += str[i];
+        }
+    }
+    return str2 + str[i];
+}
